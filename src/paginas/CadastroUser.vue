1dbf5d820351f86a15fbc4347658607e1fd65096
Paulo Henrique <paulohfprates@gmil.com>
2019-07-14 18:58:55 -0300

    css menulateral

--- src/paginas/CadastroUser.vue
+++ src/paginas/CadastroUser.vue
@@ -94,7 +94,7 @@ export default {
   input:-webkit-autofill:hover, 
   input:-webkit-autofill:focus, 
   input:-webkit-autofill:active  {
-    -webkit-box-shadow: 0 0 0 30px white inset !important;
+    box-shadow: 0 0 0 30px white inset !important;
     -webkit-text-fill-color: grey;
 }
 </style>
